83a8dffe13554826f3b3bc5fade858d8a14c5213
LTluttmann <laurin.luttmann@gmail.com>
2020-08-07 22:56:48 +0200

    changes to greedy heuristic

--- instance_demo/instance_demo.py
+++ instance_demo/instance_demo.py
@@ -18,6 +18,7 @@ from operator import itemgetter, attrgetter
 from xml.dom import minidom
 import rafs_instance as instance
 from collections import defaultdict, Iterable
+from collections import defaultdict, Iterable
 
 
 layoutFile = r'data/layout/1-1-1-2-1.xlayo'
@@ -32,9 +33,7 @@ storagePolicies['dedicated'] = 'data/sku24/pods_items_dedicated_1.txt'
 
 orders = {}
 orders['10_5'] = r'data/sku24/orders_10_mean_5_sku_24.xml'
-
-
-# orders['20_5']=r'data/sku24/orders_20_mean_5_sku_24.xml'
+# orders['20_5'] = r'data/sku24/orders_20_mean_5_sku_24.xml'
 
 class WarehouseDateProcessing():
     def __init__(self, warehouseInstance, batch_size=None):
@@ -176,7 +175,7 @@ class Demo():
                 item_id = trie[product][color]
                 self.item_id_pod_id_dict[item_id] = key
 
-    # define get functions for better readability
+    # define get functions for better readability and accessibility of data
     def get_items_by_order(self, order_idx):
         item_ids = []
         item_idx_keys = list(self.warehouseInstance.Orders[order_idx].Positions.keys())
@@ -200,21 +199,6 @@ class Demo():
         shelf2 = self.item_id_pod_id_dict[item2]
         return self.distance_ij[shelf1, shelf2]
 
-    def get_dist_of_order_to_pack(self, order_idx, retrieve_station_only=False):
-        items = self.get_items_by_order(order_idx)
-        order_pack_dists = dict()
-        for pack_station in list(self.warehouseInstance.OutputStations.keys()):
-            item_dists = []
-            for item in items:
-                item_dists.append(self.distance_ij[pack_station, item])
-            order_pack_dists[pack_station] = np.sum(item_dists)
-        if retrieve_station_only:
-            return min(order_pack_dists.keys(), key=(lambda k: order_pack_dists[k]))
-        else:
-            station = min(order_pack_dists.keys(), key=(lambda k: order_pack_dists[k]))
-            dictio = {station: order_pack_dists[station]}
-            return dictio
-
     def get_fitness_of_tour(self, tour, pack_station):
         complete_tour = [pack_station] + tour + [pack_station]
         distances = []
@@ -234,22 +218,64 @@ class GreedyHeuristic(Demo):
     def __init__(self):
         super(GreedyHeuristic, self).__init__()
         self.fill_item_id_pod_id_dict()
+        self.warehouseInstance.Orders = {str(key): value for key, value in enumerate(self.warehouseInstance.Orders)}
+
+    def get_station_with_min_total_distance(self, order_idx, retrieve_station_only=False):
+        """
+        function to retrieve the station which minimizes the total distance of the items of the
+        specified order
+        :param order_idx: index or the order
+        :param retrieve_station_only: indicates whether to return the station with min distance only or
+        a together with the distance which can be used for further decision making.
+        :return:
+        """
+        items = self.get_items_by_order(order_idx)
+        order_pack_dists = dict()
+        for pack_station in list(self.warehouseInstance.OutputStations.keys()):
+            item_dists = []
+            for item in items:
+                item_dists.append(self.distance_ij[pack_station, item])
+            order_pack_dists[pack_station] = np.sum(item_dists)
+        if retrieve_station_only:
+            return min(order_pack_dists.keys(), key=(lambda k: order_pack_dists[k]))
+        else:
+            # eventually we want the assignment of orders to stations to be balanced,
+            # then the distance can be used to make the assignment in a greedy manner until
+            # the max number of orders that may be assigned to a station is reached
+            station = min(order_pack_dists.keys(), key=(lambda k: order_pack_dists[k]))
+            dictio = {station: order_pack_dists[station]}
+            return dictio
 
     def assign_orders_to_stations(self):
+        """
+        assigns an order to a given station according to the minimum total distance. May lead to unbalanced
+        assignments, e.g. that all orders are assigned to only one station.
+        :return:
+        """
         orders_of_stations = defaultdict(list)
-        for i, order in enumerate(self.warehouseInstance.Orders):
-            pack_of_order = self.get_dist_of_order_to_pack(i, True)
-            orders_of_stations[pack_of_order].append(i)
+        for order_id, order in self.warehouseInstance.Orders.items():  # todo: give orders an ID instead of enumeration
+            station_of_order = self.get_station_with_min_total_distance(order_id, True)
+            orders_of_stations[station_of_order].append(order_id)
         return orders_of_stations
 
     def count_min_distances_for_order(self, order_idx, forbidden=None):
+        """
+        greedy: for any item of the current batch (specified in order_idx) the order which has an item that
+        minimizes the distance to that item over all items of all unassigned order of the same station is
+        determined (alternatively we can measure the total minimum distance of the items of an order to
+        all the items in the current batch --> might be more informative)
+        :param order_idx:
+        :param forbidden:
+        :return:
+        """
         if forbidden is None:
-            forbidden = []
+            forbidden = []  # to exclude orders that don´t fit in the batch anymore due to limited capacity
         order_idx = [order_idx] if not isinstance(order_idx, list) else order_idx
-        pack_assignments = self.assign_orders_to_stations()
-        station_of_order = [k for k, v in pack_assignments.items() if any([idx in v for idx in order_idx])][0]
-        other_orders_of_station = set(pack_assignments[station_of_order]).difference(set(order_idx + forbidden))
-        print("other orders in the same station as this order: ", other_orders_of_station)
+        station_assignments = self.assign_orders_to_stations()
+        station_of_order = [k for k, v in station_assignments.items() if any([idx in v for idx in order_idx])][0]
+        other_orders_of_station = list(set(station_assignments[station_of_order]).difference(set(order_idx+forbidden)))
+        print("other unassigned orders in the same station ({}) as this order: ".format(station_of_order),
+              other_orders_of_station)
         count_min_dist_items_to_order = defaultdict(int)
         min_dist_to_item = dict()
         for idx in order_idx:
@@ -266,33 +292,65 @@ class GreedyHeuristic(Demo):
                     min(min_dist_to_item_for_order.keys(), key=(lambda k: min_dist_to_item_for_order[k]))] += 1
         return max(count_min_dist_items_to_order.keys(), key=(lambda k: count_min_dist_items_to_order[k]))
 
+    def get_all_items_of_batch(self, batch):
+        items = []
+        for order in batch:
+            items.extend(self.get_items_by_order(order))
+        return items
+
+    def greedy_next_order_to_batch(self, batch, forbidden=None):
+        if forbidden is None:
+            forbidden = []  # to exclude orders that don´t fit in the batch anymore due to limited capacity
+        batch = [batch] if not isinstance(batch, list) else batch
+        station_assignments = self.assign_orders_to_stations()
+        station_of_order = [k for k, v in station_assignments.items() if any([idx in v for idx in batch])][0]
+        other_orders_of_station = list(
+            set(station_assignments[station_of_order]).difference(set(batch + forbidden)))
+        print("other unassigned orders in the same station ({}) as this order: ".format(station_of_order),
+              other_orders_of_station)
+        sum_min_dist_to_item = dict()
+        for order in other_orders_of_station:
+            min_distances = []
+            for item_in_batch in self.get_all_items_of_batch(batch):
+                min_distances.append(min([self.distance_ij[item_in_batch, item]
+                                          for item in self.get_items_by_order(order)]))
+            sum_min_dist_to_item[order] = np.mean(min_distances)  # average instead? otherwise we always pick small orders
+        return min(sum_min_dist_to_item.keys(), key=(lambda k: sum_min_dist_to_item[k]))
+
     def assign_orders_to_batches(self, pack_station_idx):
-        orders_of_stations = self.assign_orders_to_stations()
-        orders_of_stations = orders_of_stations[pack_station_idx]
+        orders_of_station = self.assign_orders_to_stations()[pack_station_idx]
         already_assigned = list()
-        assign_to_batch = defaultdict(list)
+        orders_of_batch = defaultdict(list)
         items_of_batch = defaultdict(list)
         batch_count = 0
-        while len(already_assigned) != len(orders_of_stations):
-            init_order = np.random.choice(list(set(orders_of_stations).difference(already_assigned)))
-            assign_to_batch[batch_count].append(init_order)
+        while len(already_assigned) != len(orders_of_station):
+            np.random.seed(14213)
+            init_order = np.random.choice(list(set(orders_of_station).difference(already_assigned)))
+            orders_of_batch[batch_count].append(init_order)
             already_assigned.append(init_order)
-            kappa = self.get_total_weight_by_order(init_order)
+            curr_weight = self.get_total_weight_by_order(init_order)
             forbidden_for_batch = []
-            while kappa < self.batch_weight and not len(set(already_assigned).union(forbidden_for_batch))==len(orders_of_stations):
-                new_order = self.count_min_distances_for_order(already_assigned, forbidden_for_batch)
-                if (kappa + self.get_total_weight_by_order(new_order)) <= self.batch_weight:
+            while curr_weight < self.batch_weight and not len(
+                    set(already_assigned).union(forbidden_for_batch)) == len(orders_of_station):
+                new_order = self.greedy_next_order_to_batch(already_assigned, forbidden_for_batch)
+                # new_order = self.count_min_distances_for_order(already_assigned, forbidden_for_batch)
+                print("Chosen order: ", new_order)
+                if (curr_weight + self.get_total_weight_by_order(new_order)) <= self.batch_weight:
+                    print("and it also fits in the current batch ({})".format(batch_count))
                     already_assigned.append(new_order)
-                    assign_to_batch[batch_count].append(new_order)
+                    orders_of_batch[batch_count].append(new_order)
+                    curr_weight += self.get_total_weight_by_order(new_order)
                 else:
+                    print("but it would add too much weight to the batch, go on to next...")
                     forbidden_for_batch.append(new_order)
                     continue
-            for order in assign_to_batch[batch_count]:
+            for order in orders_of_batch[batch_count]:
                 items_of_batch[batch_count].extend(self.get_items_by_order(order))
             batch_count += 1
+        print("The assignment of orders to batches looks as follows: ", orders_of_batch)
         return items_of_batch
 
-    def greedy_heuristic(self):
+    def apply_greedy_heuristic(self):
         tours_of_pack_station = dict()
         for pack_station in list(self.warehouseInstance.OutputStations.keys()):
             tour_of_batch = dict()
@@ -301,7 +359,7 @@ class GreedyHeuristic(Demo):
                 tour = []
                 distances_to_pack = {item: self.distance_ij[pack_station, self.item_id_pod_id_dict[item]] for item in items}
                 min_item = min(distances_to_pack.keys(), key=(lambda k: distances_to_pack[k]))
-                tour.append(min_item) # TODO in items tauchen doppelte einträge auf. Wie geht man damit um?
+                tour.append(min_item)  # TODO in items tauchen doppelte einträge auf. Wie geht man damit um?
                 while len(tour) != len(items):
                     distances_to_current_item = {
                         item: self.distance_ij[tour[-1], self.item_id_pod_id_dict[item]]
@@ -316,5 +374,8 @@ class GreedyHeuristic(Demo):
 
 if __name__ == "__main__":
     greedy = GreedyHeuristic()
-    solution = greedy.greedy_heuristic()
+    starttime = time.time()
+    solution = greedy.apply_greedy_heuristic()
+    print("runtime: ", time.time() - starttime)
     print("fitness: ", greedy.get_fitness_of_solution(solution))
+    print(solution)
